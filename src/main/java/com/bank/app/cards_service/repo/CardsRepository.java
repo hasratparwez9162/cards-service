808915d299dc47c8388355ae2191c8f58393ac1d
Hasrat Parwez <hasratp@aditiconsulting.com>
2024-08-22 13:32:30 +0530

    first commit

---
+++ src/main/java/com/bank/app/cards_service/repo/CardsRepository.java
@@ -0,0 +1,9 @@
+package com.bank.app.cards_service.repo;
+
+import com.bank.app.cards_service.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards,Long> {
+}
