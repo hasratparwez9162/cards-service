07426a928ef62d74288c01650d6545a5af6fef22
Hasrat Parwez <91671497+hasratparwez9162@users.noreply.github.com>
2024-08-28 12:37:22 +0530

    Comment according to end points

--- src/main/java/com/bank/app/cards_service/controller/CardController.java
+++ src/main/java/com/bank/app/cards_service/controller/CardController.java
@@ -13,24 +13,27 @@ import java.util.List;
 public class CardController {
     @Autowired
     private CardService cardService;
-
+//    Issue Cards like credit and Debit Cards
     @PostMapping("/issue")
     public ResponseEntity<Card> issueCard(@RequestBody Card card) {
         Card newCard = cardService.issueCard(card);
         return new ResponseEntity<>(newCard, HttpStatus.CREATED);
     }
-
+//    Get cards details of a user
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Card>> getCardsByUserId(@PathVariable Long userId) {
         List<Card> cards = cardService.getCardsByUserId(userId);
         return new ResponseEntity<>(cards, HttpStatus.OK);
     }
 
+//    block Card by cardId
+
     @PutMapping("/block/{cardId}")
     public ResponseEntity<String> blockCard(@PathVariable Long cardId) {
         cardService.blockCard(cardId);
         return new ResponseEntity<>("Card blocked successfully", HttpStatus.OK);
     }
+//    Unblock Cards
     @PutMapping("/unblock/{cardId}")
     public ResponseEntity<String> unblockCard(@PathVariable Long cardId) {
         cardService.unblockCard(cardId);
